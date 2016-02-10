592d1d08bae5ebde7462244174a6c5a4a003e9b1
waso96 <wasoarun@gmail.com>
2016-02-10 10:25:14 +0700

    init

---
+++ src/kbTomcat/Getpath.java
@@ -0,0 +1,27 @@
+package kbTomcat;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/Getpath")
+public class Getpath extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public Getpath() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().append("Served at: ").append(System.getProperty("catalina.base"));
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
